6f01ad35f464f19257a03c519e0fceb058c7efa8
Kevin Ramharak <kevin.ramharak@infocaster.net>
2018-09-22 19:42:37 +0200

    add source code

---
+++ dist/ng-sane-defaults.js
@@ -0,0 +1,54 @@
+(function () {
+    angular.module('ng-sane-defaults', []).config([
+        '$compileProvider',
+        function configure($compileProvider) {
+            // get the original `registerComponent` function
+            var registerComponent = $compileProvider.component;
+            // override the original function
+            $compileProvider.component = component;
+            /**
+             *
+             * @param nameOrDict name or Dictionary of component names and their options
+             * @param options options for the component if the first parameter is the name
+             */
+            function component(nameOrDict, options) {
+                var _a;
+                // make sure the function signature is correct, if not pass it trough to the original function to handle errors
+                if (typeof nameOrDict === 'string' && typeof options !== 'object')
+                    return registerComponent.call(this, nameOrDict, options);
+                if (typeof nameOrDict !== 'string' && typeof nameOrDict !== 'object')
+                    return registerComponent.call(this, nameOrDict, options);
+                // create the dictionary call signature as it is easier to work with
+                var components = (typeof nameOrDict === 'string') ? (_a = {}, _a[nameOrDict] = options, _a) : nameOrDict;
+                var _loop_1 = function (key) {
+                    var options_1 = components[key];
+                    // check if it has a controller function
+                    if (typeof options_1.controller !== 'function')
+                        return "continue";
+                    // check if it has a `static $defaults` property, use `getOwnPropertyNames` to check for a getter function without invoking it
+                    if (Object.getOwnPropertyNames(options_1.controller).indexOf('$defaults') === -1)
+                        return "continue";
+                    // get the original `$onInit` function or undefined if there was none
+                    var onInit = options_1.controller.prototype.$onInit;
+                    // patch the `$onInit` function
+                    options_1.controller.prototype.$onInit = function $onInit() {
+                        var defaults = options_1.controller.$defaults;
+                        if (typeof defaults === 'object') {
+                            for (var prop in defaults) {
+                                if (this[prop] == null) {
+                                    this[prop] = defaults[prop];
+                                }
+                            }
+                        }
+                        // call the lifecycle hook if it existed
+                        return (typeof onInit === 'function') ? onInit.call(this) : void 0;
+                    };
+                };
+                for (var key in components) {
+                    _loop_1(key);
+                }
+                return registerComponent.call(this, components);
+            }
+        }
+    ]);
+})();
---
+++ src/ng-sane-defaults.module.ts
@@ -0,0 +1,63 @@
+(function() {
+    type Dict<T = any> = { [key: number]: T, [key: string]: T };
+
+    angular.module('ng-sane-defaults', []).config([
+        '$compileProvider',
+        function configure($compileProvider: angular.ICompileProvider) {
+            // get the original `registerComponent` function
+            const registerComponent = $compileProvider.component;
+            
+            // override the original function
+            $compileProvider.component = component;
+
+            /**
+             * 
+             * @param nameOrDict name or Dictionary of component names and their options
+             * @param options options for the component if the first parameter is the name
+             */
+            function component(this: angular.ICompileProvider, nameOrDict: string|Dict<angular.IComponentOptions>, options?: angular.IComponentOptions) {
+                // make sure the function signature is correct, if not pass it trough to the original function to handle errors
+                if (typeof nameOrDict === 'string' && typeof options !== 'object')
+                    return registerComponent.call(this, nameOrDict, options as any);
+                if (typeof nameOrDict !== 'string' && typeof nameOrDict !== 'object')
+                    return registerComponent.call(this, nameOrDict, options as any);
+
+                // create the dictionary call signature as it is easier to work with
+                const components: Dict<angular.IComponentOptions> = (typeof nameOrDict === 'string') ? { [nameOrDict]: options! } : nameOrDict;
+
+                for (const key in components) {
+                    const options = components[key];
+
+                    // check if it has a controller function
+                    if (typeof options.controller !== 'function')
+                        continue;
+                    
+                    // check if it has a `static $defaults` property, use `getOwnPropertyNames` to check for a getter function without invoking it
+                    if (Object.getOwnPropertyNames(options.controller).indexOf('$defaults') === -1)
+                        continue;
+
+                    // get the original `$onInit` function or undefined if there was none
+                    const onInit: Function|undefined = options.controller.prototype.$onInit;
+                    
+                    // patch the `$onInit` function
+                    options.controller.prototype.$onInit = function $onInit() {
+                        const defaults = (options.controller as Dict).$defaults;
+
+                        if (typeof defaults === 'object') {
+                            for (const prop in defaults) {
+                                if (this[prop] == null) {
+                                    this[prop] = defaults[prop];
+                                }
+                            }
+                        }
+
+                        // call the lifecycle hook if it existed
+                        return (typeof onInit === 'function') ? onInit.call(this) : void 0;
+                    };
+                }
+                
+                return registerComponent.call(this, components);
+            }
+        }
+    ]);
+})();
