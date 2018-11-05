8aaa64e3b31ecfa5658619e0d98713ce7f33e6f8
Kevin Ramharak <kevin@ramharak.nl>
2018-11-05 14:03:06 +0100

    single quotes

--- src/ng-sane-defaults.module.ts
+++ src/ng-sane-defaults.module.ts
@@ -2,8 +2,8 @@
     type Dict<T = any> = { [key: number]: T; [key: string]: T };
 
     angular
-        .module("ng-sane-defaults", [])
-        .config(["$compileProvider", configure]);
+        .module('ng-sane-defaults', [])
+        .config(['$compileProvider', configure]);
 
     function configure($compileProvider: angular.ICompileProvider) {
         $compileProvider.component = patchRegisterComponent(
@@ -12,7 +12,7 @@
     }
 
     function patchRegisterComponent(
-        registerComponent: angular.ICompileProvider["component"]
+        registerComponent: angular.ICompileProvider['component']
     ) {
         return component;
         /**
@@ -26,17 +26,17 @@
             options?: angular.IComponentOptions
         ) {
             // make sure the function signature is correct, if not pass it trough to the original function to handle errors
-            if (typeof nameOrDict === "string" && typeof options !== "object")
+            if (typeof nameOrDict === 'string' && typeof options !== 'object')
                 return registerComponent.call(this, nameOrDict, options as any);
             if (
-                typeof nameOrDict !== "string" &&
-                typeof nameOrDict !== "object"
+                typeof nameOrDict !== 'string' &&
+                typeof nameOrDict !== 'object'
             )
                 return registerComponent.call(this, nameOrDict, options as any);
 
             // create the dictionary call signature as it is easier to work with
             const components: Dict<angular.IComponentOptions> =
-                typeof nameOrDict === "string"
+                typeof nameOrDict === 'string'
                     ? { [nameOrDict]: options! }
                     : nameOrDict;
 
@@ -44,12 +44,12 @@
                 const options = components[key];
 
                 // check if it has a controller function
-                if (typeof options.controller !== "function") continue;
+                if (typeof options.controller !== 'function') continue;
 
                 // check if it has a `static $defaults` property, use `getOwnPropertyNames` to check for a getter function without invoking it
                 if (
                     Object.getOwnPropertyNames(options.controller).indexOf(
-                        "$defaults"
+                        '$defaults'
                     ) === -1
                 )
                     continue;
@@ -62,7 +62,7 @@
                 options.controller.prototype.$onInit = function $onInit() {
                     const defaults = (options.controller as Dict).$defaults;
 
-                    if (typeof defaults === "object") {
+                    if (typeof defaults === 'object') {
                         for (const prop in defaults) {
                             if (this[prop] == null) {
                                 this[prop] = defaults[prop];
@@ -71,7 +71,7 @@
                     }
 
                     // call the lifecycle hook if it existed
-                    return typeof onInit === "function"
+                    return typeof onInit === 'function'
                         ? onInit.call(this)
                         : void 0;
                 };
